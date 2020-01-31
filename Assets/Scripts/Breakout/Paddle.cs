40936894405837794e5436539d6153436e6679fc
Jacob Cohen <JacobCohenIsrael@gmail.com>
2020-01-31 11:06:20 +0200

    Initial Paddle Logic

---
+++ Assets/Scripts/Breakout/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    private PaddleSettings paddleSettings;
+
+    private Vector3 nextPosition;
+
+    private Ball ball;
+
+
+    private void Start()
+    {
+        ball = FindObjectOfType<Ball>();
+    }
+
+    private void Update()
+    {
+        nextPosition = Vector2.MoveTowards(transform.position, ball.gameObject.transform.position, paddleSettings.GetSpeed() * Time.deltaTime);
+        nextPosition.y = transform.position.y;
+        transform.position = nextPosition;
+    }
+}
