2848734eec60f4fbdafa9276a7c5634c9efa9175
teh1archon <teh1archon@gmail.com>
2020-01-31 08:59:08 +0200

    1st push

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+	[SerializeField] Sprite[] breakageSprites;
+	[SerializeField] SpriteRenderer breakageRenderer;
+
+	int hits = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		breakageRenderer.sprite = breakageSprites[hits];
+    }
+
+	public void BreakBlock()
+	{
+		hits++;
+		if (hits == breakageSprites.Length)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			breakageRenderer.sprite = breakageSprites[hits];
+		}
+	}
+
+	public void FixBlock()
+	{
+		if (hits > 0)
+		{
+			hits--;
+			breakageRenderer.sprite = breakageSprites[hits];
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public List<Block> blocksList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Block[] blocksArray = FindObjectsOfType<Block>();
+		foreach(Block block in blocksArray)
+		{
+			blocksList.Add(block);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		TestBreakage();
+    }
+
+	// FOR DEBUGGING
+	private void TestBreakage()
+	{
+		if(Input.GetKeyDown(KeyCode.Space) && blocksList[0] != null)
+		{
+			blocksList[0].BreakBlock();
+		}
+	}
+}
